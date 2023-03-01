de2f356d0c7400a014568d8709a72a0b25b6328d
Сергей <74443835+SergeyPegas@users.noreply.github.com>
2023-03-01 21:51:01 +0300

    Feature/13 create new workspace (#15)
    * test/develop
    
    * ISSUE-13: Подготовка рабочего пространства
    
    * ISSUE-13/2: Подготовка рабочего пространства
    
    ---------
    
    Co-authored-by: lirostin <lirostin@gmail.com>

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+ADMIN = 'admin'
+MODERATOR = 'moderator'
+USER = 'user'
+
+ROLE_CHOICES = [(ADMIN, ADMIN), (MODERATOR, MODERATOR), (USER, USER), ]
+
+
+class User(AbstractUser):
+    """Абстрактная модель пользователя."""
+
+
+class Category(models.Model):
+    """Модель категорий произведений."""
+
+
+class Genre(models.Model):
+    """Модель жанров произведений."""
+
+
+class Title(models.Model):
+    """Модель произведений."""
+
+
+class GenreTitle(models.Model):
+    """Модель связи жанра и произведения."""
+
+
+class Review(models.Model):
+    """Модель отзывов о произведении."""
+
+
+class Comment(models.Model):
+    """Модель - комментарии к отзывам."""
+
+# Создал комментарий для commit
