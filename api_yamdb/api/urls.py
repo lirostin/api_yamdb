60983035b304193f8a37a5c069afb90fc00a3cb5
Сергей Плевако <paviliomm@mail.ru>
2023-03-04 13:03:03 +0300

    ISSUE-6/1: Модели, view и эндпойнты для категорий

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from .views import GenreViewSet
+from .views import CategoryViewSet, GenreViewSet
 
 app_name = 'api'
 
 router_v1 = SimpleRouter()
 
 router_v1.register('genres', GenreViewSet, basename='genre')
+router_v1.register('categories', CategoryViewSet, basename='category')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls), name='api'),
